629f3f2c000499fa982bc0f51cf0a6a293c0169a
Vinod Kumar <vinodmr@hcl.com>
2018-08-29 06:38:13 +0000

    Initial Import UTS

---
+++ uts/ms/helloworld-usa/src/main/java/rnd/web/service/rest/App.java
@@ -0,0 +1,14 @@
+package rnd.web.service.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+	    System.getProperties().put( "server.port", 8085);
+		SpringApplication.run(new Object[] { AppConfig.class, App.class }, args);
+	}
+
+}
